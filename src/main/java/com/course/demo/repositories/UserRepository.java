27bd31934e3bc984a000bea0bc324ce02810f409
romeu3r <romeu3r@gmail.com>
2022-02-21 17:14:24 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/course/demo/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package com.course.demo.repositories;
+
+import com.course.demo.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}

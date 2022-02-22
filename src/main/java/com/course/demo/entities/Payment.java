76b9f25510509d063acdd9efb159874fd1699b25
romeu3r <romeu3r@gmail.com>
2022-02-22 08:44:56 -0300

    Subtotal & Total methods

--- src/main/java/com/course/demo/entities/Payment.java
+++ src/main/java/com/course/demo/entities/Payment.java
@@ -1,5 +1,7 @@
 package com.course.demo.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
 import java.io.Serializable;
 import java.time.Instant;
@@ -12,6 +14,7 @@ public class Payment implements Serializable {
     private Long id;
     private Instant moment;
 
+    @JsonIgnore
     @OneToOne
     @MapsId
     private Order order;
